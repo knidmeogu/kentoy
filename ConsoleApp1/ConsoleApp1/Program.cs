d67a8485587b9ce4264d34299995d6c289d36d35
unknown <Admin@CSLAB250.CSSERVER.local>
2024-02-01 19:40:40 +0800

    feb 1 2023

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HelloWorld
+{
+    public class Math
+    {
+        public void add(int num1, int num2)
+        {
+            Console.WriteLine("The sum of num1 and num2 is " + (num1 + num2));
+        }
+
+        public void sub(int num1, int num2)
+        {
+            Console.WriteLine("The difference of num1 and num2 is " + (num1 - num2));
+        }
+
+        public void mul(int num1, int num2)
+        {
+            Console.WriteLine("The product of num1 and num2 is " + (num1 * num2));
+        }
+
+        public void div(int num1, int num2)
+        {
+            Console.WriteLine("The quotient of num1 and num2 is " + (num1 / num2));
+        }
+    }
+    class Program
+    {   
+
+        static void Main(string[] args)
+        {
+            Math m = new Math();
+            Console.Write("Enter num1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter num2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            /*Console.WriteLine("+ - * /");
+            Console.Write("Enter choice: ");
+            String choice = (Console.ReadLine());
+
+            switch (choice)
+            {
+                case "+":
+                    Console.WriteLine(m.add);
+                    break;
+                case "-":
+                    Console.WriteLine(m.sub);
+                    break;
+                case "*":
+                    Console.WriteLine(m.mul);
+                    break;
+                case "/":
+                    Console.WriteLine(m.div);
+                    break;
+                default:
+                    Console.WriteLine("INVALID CHOICE!!!");
+                    break;
+            }*/
+
+            m.add(num1, num2);
+            m.sub(num1, num2);
+            m.mul(num1, num2);
+            m.div(num1, num2);
+        }
+    }
+}
