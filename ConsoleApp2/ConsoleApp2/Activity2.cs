0fbbd5a6ffb1ea1102890995e9e428e75a6c895e
unknown <Admin@CSLAB250.CSSERVER.local>
2024-02-06 19:27:08 +0800

    020624 - Activity 2 - Loop

---
+++ ConsoleApp2/ConsoleApp2/Activity2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HelloWorld2
+{
+    public class Stututu
+    { 
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+    }
+
+    class Activity2
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
